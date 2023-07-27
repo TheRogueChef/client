796d1fb656f22ec931d153032953b2001ecb6cd0
Erik Carlson <theroguechef@comcast.net>
2023-07-27 13:10:50 -0700

    Cleaned up Spotify

--- src/components/tribes/music/MusicPlayer.js
+++ src/components/tribes/music/MusicPlayer.js
@@ -2,7 +2,7 @@
 const MusicPlayer = () => {
     return (
         <div>
-            <iframe style={{ borderRadius: '1%' }} src="https://open.spotify.com/embed/playlist/37i9dQZF1E36bDMUlfbeI4?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+            <iframe title="OnionMix" style={{borderRadius:"1%"}} src="https://open.spotify.com/embed/playlist/5NbIfVQKtuFcGdeHmdaQj1?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
             <br/>
         </div>
     );
